149f485d6309685943adcad3b0ce78af1788bf62
Sergey Tolkachyov <musstudent@mail.ru>
2025-03-03 12:10:03 +0400

    v.1.1.2 Changing the parameter of the `slidesPerView`. Now you can specify fractional numbers in the `slidesPerView`  parameter so that part of the next slide is visible.

--- src/Helper/WtjshoppingswipercarouselHelper.php
+++ src/Helper/WtjshoppingswipercarouselHelper.php
@@ -2,23 +2,24 @@
 /**
  * @package     WT JShopping Swiper carousel
  * @copyright   Copyright (C) 2022-2023 Sergey Tolkachyov. All rights reserved.
- * @author      Sergey Tolkachyov - https://web-tolk.ru
+ * @author      Sergey Tolkachyov
  * @link        https://web-tolk.ru
- * @version     1.1.1
+ * @version     1.1.2
  * @license     GNU General Public License version 3 or later
  */
 
 namespace Joomla\Module\Wtjshoppingswipercarousel\Site\Helper;
 
-use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\Component\Jshopping\Site\Table\ConfigTable;
-use Joomla\Filesystem\Folder;
+use Joomla\Component\Jshopping\Site\Lib\JSFactory;
 use Joomla\Component\Jshopping\Site\Model\Productlist;
+use Joomla\Component\Jshopping\Site\Table\ConfigTable;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
+use stdClass;
 
-
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
 
 /**
@@ -49,6 +50,218 @@ class WtjshoppingswipercarouselHelper
 	}
 
 	/**
+	 * @param $params
+	 *
+	 * @return array images
+	 *
+	 * @since 1.0.0
+	 */
+	private function getImagesFromFolder($params): array
+	{
+		$images            = [];
+		$files_from_folder = Folder::files(JPATH_ROOT . '/images/' . $params->get('folder'));
+		$images_extensions = ['bmp', 'gif', 'jpg', 'png', 'jpeg', 'webp', 'avif'];
+		foreach ($files_from_folder as $file)
+		{
+			if (in_array(File::getExt($file), $images_extensions))
+			{
+				$image       = new stdClass();
+				$image->path = 'images/' . $params->get('folder') . '/' . $file;
+				$image->alt  = $params->get('folder_images_alt');
+				$images[]    = $image;
+			}
+		}
+
+		return $images;
+	}
+
+	/**
+	 *
+	 * @param $params
+	 *
+	 * @return array
+	 *
+	 * @since 1.0.0
+	 */
+	private function getImagesFromModuleParams($params): array
+	{
+		$list = [];
+		foreach ($params->get("fields") as $field)
+		{
+			$image            = new stdClass();
+			$clean_image_path = HTMLHelper::cleanImageURL($field->image);
+			$image->path      = $clean_image_path->url;
+			$image->alt       = $field->image_alt;
+			$list[]           = $image;
+		}
+
+		return $list;
+	}
+
+	/**
+	 * Return JoomShopping product list
+	 *
+	 * @param $params
+	 * @param $app
+	 *
+	 * @return array
+	 *
+	 * @since 1.0.0
+	 */
+	private function getJshoppingProducts($params, $app): array
+	{
+		PluginHelper::importPlugin('jshoppingproducts');
+		$jshopConfig = $this->getJshopConfig();
+		$noimage     = $jshopConfig->image_product_live_path . "/noimage.gif";
+		$filters     = [];
+		$count       = $params->get('joomshopping_products_count_products', 4);
+		$list        = [];
+		if ($params->get('joomshopping_products_type') == 'last_products')
+		{
+			$productlist = JSFactory::getModel('last', 'Site\\Productlist');
+			$categories  = $params->get('catids', []);
+			if (is_array($categories) && count($categories) > 0)
+			{
+				$categories = array_map('intval', $categories);
+			}
+			$filters['categorys'] = $categories;
+
+			$order = 'prod.product_id';
+//		$order = 'prod.'.$params->get('order_by', 'product_id');
+			$rows = $productlist->getLoadProducts($filters, $order, 'DESC', 0, $count);
+
+			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
+			$view       = new stdClass();
+			$view->rows = $rows;
+			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$productlist]);
+			$list = $view->rows;
+
+		}
+
+		if ($params->get('joomshopping_products_type') == 'products_by_ids')
+		{
+			$product_ids = $params->get('joomshopping_products_ids');
+
+			if (!empty($product_ids))
+			{
+				if (strpos($product_ids, ',') !== false)
+				{
+					$product_ids = explode(',', $product_ids);
+					$product_ids = array_map('trim', $product_ids);
+
+				}
+				else
+				{
+					$product_ids = (array) trim($product_ids);
+				}
+
+				if (is_array($product_ids) && count($product_ids) > 0)
+				{
+
+					$product_list = new Productlist\ListModel();
+
+					/**
+					 * @method getLoadProducts()
+					 * @var $filters               array - ['products'=> $product_ids]
+					 * @var $order                 string
+					 * @var $order_by              string
+					 * @var $limitstart            int
+					 * @var $limit                 int
+					 * @var $listProductUpdateData bool
+					 */
+					$list = $product_list->getLoadProducts(['products' => $product_ids], 'FIELD(prod.product_id, ' . implode(',', $product_ids) . ')', null, 0, 0, 1);
+				}
+			}
+		}
+
+		if ($params->get('joomshopping_products_type') == 'toprating_products')
+		{
+			$product    = \JSFactory::getModel('toprating', 'Site\\Productlist');
+			$categories = $params->get('catids', []);
+			if (is_array($categories) && count($categories) > 0)
+			{
+				$categories = array_map('intval', $categories);
+			}
+			$filters['categorys'] = $categories;
+			$rows                 = $product->getLoadProducts($filters, null, 'DESC', 0, $count);
+
+			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
+			$view       = new stdClass();
+			$view->rows = $rows;
+			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$product]);
+			$list = $view->rows;
+
+		}
+		if ($params->get('joomshopping_products_type') == 'tophits_products')
+		{
+			$product    = \JSFactory::getModel('tophits', 'Site\\Productlist');
+			$categories = $params->get('catids', []);
+			if (is_array($categories) && count($categories) > 0)
+			{
+				$categories = array_map('intval', $categories);
+			}
+			$filters['categorys'] = $categories;
+
+			$rows = $product->getLoadProducts($filters, null, 'DESC', 0, $count);
+
+			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
+			$view       = new stdClass();
+			$view->rows = $rows;
+			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$product]);
+			$list = $view->rows;
+
+		}
+		if ($params->get('joomshopping_products_type') == 'bestseller_products')
+		{
+			$categories = $params->get('catids', []);
+			if (is_array($categories) && count($categories) > 0)
+			{
+				$categories = array_map('intval', $categories);
+			}
+
+			$filters['categorys'] = $categories;
+			$productlist          = JSFactory::getModel('bestseller', 'Site\\Productlist');
+			$rows                 = $productlist->getLoadProducts($filters, null, 'DESC', 0, $count);
+			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
+			$view       = new stdClass();
+			$view->rows = $rows;
+			$app->triggerEvent('onBeforeDisplayProductListView', [&$view]);
+			$list = $view->rows;
+
+		}
+		if ($params->get('joomshopping_products_type') == 'label_products')
+		{
+
+			$product   = JSFactory::getModel('label', 'Site\\Productlist');
+			$label_ids = (array) $params->get('label_id');
+
+			if ($label_ids)
+			{
+				$filters['labels'] = $label_ids;
+			}
+
+			$categories = $params->get('catids', []);
+			if (is_array($categories) && count($categories) > 0)
+			{
+				$categories = array_map('intval', $categories);
+			}
+
+			$filters['categorys'] = $categories;
+
+			$rows = $product->getLoadProducts($filters, null, 'DESC', 0, $count);
+
+			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
+			$view       = new stdClass();
+			$view->rows = $rows;
+			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$product]);
+			$list = $view->rows;
+
+		}
+
+		return $list;
+	}
+
+	/**
 	 * Return Joomshopping config object
 	 *
 	 * @return object|ConfigTable|mixed
@@ -62,7 +275,7 @@ class WtjshoppingswipercarouselHelper
 			require_once(JPATH_SITE . '/components/com_jshopping/bootstrap.php');
 		}
 
-		$jshopConfig = \JSFactory::getConfig();
+		$jshopConfig = JSFactory::getConfig();
 
 		return $jshopConfig;
 	}
@@ -183,218 +396,5 @@ class WtjshoppingswipercarouselHelper
 
 		$app->getDocument()->addScriptOptions('mod_wtjshoppingswipercarousel' . $module_id, $swiper_params);
 
-	}
-
-	/**
-	 * @param $params
-	 *
-	 * @return array images
-	 *
-	 * @since 1.0.0
-	 */
-	private function getImagesFromFolder($params): array
-	{
-		$images            = [];
-		$files_from_folder = Folder::files(JPATH_ROOT . '/images/' . $params->get('folder'));
-		$images_extensions = ['bmp', 'gif', 'jpg', 'png', 'jpeg', 'webp', 'avif'];
-		foreach ($files_from_folder as $file)
-		{
-			if (in_array(File::getExt($file), $images_extensions))
-			{
-				$image       = new \stdClass();
-				$image->path = 'images/' . $params->get('folder') . '/' . $file;
-				$image->alt  = $params->get('folder_images_alt');
-				$images[]    = $image;
-			}
-		}
-
-		return $images;
-	}
-
-	/**
-	 *
-	 * @param $params
-	 *
-	 * @return array
-	 *
-	 * @since 1.0.0
-	 */
-	private function getImagesFromModuleParams($params): array
-	{
-		$list = [];
-		foreach ($params->get("fields") as $field)
-		{
-			$image            = new \stdClass();
-			$clean_image_path = HTMLHelper::cleanImageURL($field->image);
-			$image->path      = $clean_image_path->url;
-			$image->alt       = $field->image_alt;
-			$list[]           = $image;
-		}
-
-		return $list;
-	}
-
-
-	/**
-	 * Return JoomShopping product list
-	 *
-	 * @param $params
-	 * @param $app
-	 *
-	 * @return array
-	 *
-	 * @since 1.0.0
-	 */
-	private function getJshoppingProducts($params, $app): array
-	{
-		PluginHelper::importPlugin('jshoppingproducts');
-		$jshopConfig = $this->getJshopConfig();
-		$noimage     = $jshopConfig->image_product_live_path . "/noimage.gif";
-		$filters     = [];
-		$count       = $params->get('joomshopping_products_count_products', 4);
-		$list        = [];
-		if ($params->get('joomshopping_products_type') == 'last_products')
-		{
-			$productlist = \JSFactory::getModel('last', 'Site\\Productlist');
-			$categories  = $params->get('catids', []);
-			if (is_array($categories) && count($categories) > 0)
-			{
-				$categories = array_map('intval', $categories);
-			}
-			$filters['categorys'] = $categories;
-
-			$order = 'prod.product_id';
-//		$order = 'prod.'.$params->get('order_by', 'product_id');
-			$rows = $productlist->getLoadProducts($filters, $order, 'DESC', 0, $count);
-
-			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
-			$view       = new \stdClass();
-			$view->rows = $rows;
-			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$productlist]);
-			$list = $view->rows;
-
-		}
-
-		if ($params->get('joomshopping_products_type') == 'products_by_ids')
-		{
-			$product_ids = $params->get('joomshopping_products_ids');
-
-			if (!empty($product_ids))
-			{
-				if (strpos($product_ids, ',') !== false)
-				{
-					$product_ids = explode(',', $product_ids);
-					$product_ids = array_map('trim', $product_ids);
-
-				}
-				else
-				{
-					$product_ids = (array) trim($product_ids);
-				}
-
-				if (is_array($product_ids) && count($product_ids) > 0)
-				{
-
-					$product_list = new Productlist\ListModel();
-
-					/**
-					 * @method getLoadProducts()
-					 * @var $filters               array - ['products'=> $product_ids]
-					 * @var $order                 string
-					 * @var $order_by              string
-					 * @var $limitstart            int
-					 * @var $limit                 int
-					 * @var $listProductUpdateData bool
-					 */
-					$list = $product_list->getLoadProducts(['products' => $product_ids], 'FIELD(prod.product_id, ' . implode(',', $product_ids) . ')', null, 0, 0, 1);
-				}
-			}
-		}
-
-		if ($params->get('joomshopping_products_type') == 'toprating_products')
-		{
-			$product    = \JSFactory::getModel('toprating', 'Site\\Productlist');
-			$categories = $params->get('catids', []);
-			if (is_array($categories) && count($categories) > 0)
-			{
-				$categories = array_map('intval', $categories);
-			}
-			$filters['categorys'] = $categories;
-			$rows                 = $product->getLoadProducts($filters, null, 'DESC', 0, $count);
-
-			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
-			$view       = new \stdClass();
-			$view->rows = $rows;
-			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$product]);
-			$list = $view->rows;
-
-		}
-		if ($params->get('joomshopping_products_type') == 'tophits_products')
-		{
-			$product    = \JSFactory::getModel('tophits', 'Site\\Productlist');
-			$categories = $params->get('catids', []);
-			if (is_array($categories) && count($categories) > 0)
-			{
-				$categories = array_map('intval', $categories);
-			}
-			$filters['categorys'] = $categories;
-
-			$rows = $product->getLoadProducts($filters, null, 'DESC', 0, $count);
-
-			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
-			$view       = new \stdClass();
-			$view->rows = $rows;
-			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$product]);
-			$list = $view->rows;
-
-		}
-		if ($params->get('joomshopping_products_type') == 'bestseller_products')
-		{
-			$categories = $params->get('catids', []);
-			if (is_array($categories) && count($categories) > 0)
-			{
-				$categories = array_map('intval', $categories);
-			}
-
-			$filters['categorys'] = $categories;
-			$productlist          = \JSFactory::getModel('bestseller', 'Site\\Productlist');
-			$rows                 = $productlist->getLoadProducts($filters, null, 'DESC', 0, $count);
-			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
-			$view       = new \stdClass();
-			$view->rows = $rows;
-			$app->triggerEvent('onBeforeDisplayProductListView', [&$view]);
-			$list = $view->rows;
-
-		}
-		if ($params->get('joomshopping_products_type') == 'label_products')
-		{
-
-			$product   = \JSFactory::getModel('label', 'Site\\Productlist');
-			$label_ids = (array) $params->get('label_id');
-
-			if ($label_ids)
-			{
-				$filters['labels'] = $label_ids;
-			}
-
-			$categories = $params->get('catids', []);
-			if (is_array($categories) && count($categories) > 0)
-			{
-				$categories = array_map('intval', $categories);
-			}
-
-			$filters['categorys'] = $categories;
-
-			$rows = $product->getLoadProducts($filters, null, 'DESC', 0, $count);
-
-			$app->triggerEvent('onBeforeDisplayProductList', [&$rows]);
-			$view       = new \stdClass();
-			$view->rows = $rows;
-			$app->triggerEvent('onBeforeDisplayProductListView', [&$view, &$product]);
-			$list = $view->rows;
-
-		}
-
-		return $list;
 	}
 }
